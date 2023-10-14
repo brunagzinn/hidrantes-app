97027f6f017401f8b8742040ec8d0f3c50df2e6b
Bruna Zinn <brunagabrilazinn@gmail.com>
2023-10-13 23:03:22 -0300

    color

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1> Olá Next JS </h1>
+      <h1> Hidrante Fácil </h1>
     </main>
   )
 }
