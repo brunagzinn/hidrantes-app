064b63a2d597c8c746f23555351a61462d1dda30
Bruna Zinn <burnagabrielazinn@gmail.com>
2024-06-20 19:48:42 -0300

    alterado o contatos/page para user o fetch autenticado

--- app/contatos/page.js
+++ app/contatos/page.js
@@ -1,35 +1,19 @@
 "use client"
 import { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation'
-import styles from './contatos.module.css'
-import Link from 'next/link'
+import { useSearchParams } from 'next/navigation';
+import styles from './contatos.module.css';
+import Link from 'next/link';
 import Authenticator from '@/src/components/authenticator';
 import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from '@nextui-org/react';
+import useAuthenticatedFetch from '@/src/hooks/useAuthenticatedFetch';
 
-
-const baseUrl =
-  (process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000");
-
-async function buscarContatos(bairro, logradouro, nome, tipo) {
-  try {
-    const token = localStorage.getItem("token");
-    const resposta = await fetch(`${baseUrl}/api/contatos?bairro=${bairro}&logradouro=${logradouro}&nome=${nome}&tipo=${tipo}`, {
-      cache: 'no-store',
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
-    return await resposta.json();
-  } catch (erro) {
-    console.error(erro);
-    return [];
-  }
-}
+const baseUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000";
 
 export default function Page() {
-
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [contatoDetalhe, setContatoDetalhe] = useState(null);
+  const [contatos, setContatos] = useState([]);
+  const authenticatedFetch = useAuthenticatedFetch();
 
   const searchParams = useSearchParams();
   const searchBairro = searchParams.get('bairro');
@@ -40,42 +24,55 @@ export default function Page() {
   const [logradouro, setLogradouro] = useState(searchLogradouro ?? '');
   const [nome, setNome] = useState(searchNome ?? '');
   const [tipo, setTipo] = useState(searchTipo ?? '');
-  const [contatos, setContatos] = useState([]);
 
   useEffect(() => {
-    buscarContatos(bairro, logradouro, nome, tipo).then(results => {
-      setContatos(results);
-    })
-  }, [bairro, logradouro, nome, tipo])
+    const fetchContatos = async () => {
+      try {
+        const response = await authenticatedFetch(`${baseUrl}/api/contatos?bairro=${bairro}&logradouro=${logradouro}&nome=${nome}&tipo=${tipo}`, {
+          cache: 'no-store',
+        });
+        if (response.ok) {
+          const results = await response.json();
+          setContatos(results);
+        } else {
+          console.error("Failed to fetch contacts", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error fetching contacts", error);
+      }
+    };
+
+    fetchContatos();
+  }, [bairro, logradouro, nome, tipo, authenticatedFetch]);
+
   return (
     <div className={styles.container}>
-
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className='flex flex-col gap-1'>{contatoDetalhe.nome}</ModalHeader>
+              <ModalHeader className='flex flex-col gap-1'>{contatoDetalhe?.nome}</ModalHeader>
               <ModalBody>
                 <div>
-                  <p><strong>Nome: </strong>{contatoDetalhe.nome}</p>
-                  <p><strong>Logradouro: </strong>{contatoDetalhe.endereco}</p>
-                  <p><strong>Bairro: </strong>{contatoDetalhe.bairro}</p>
-                  <p><strong>Cidade: </strong>{contatoDetalhe.cidade}</p>
-                  <p><strong>Estado: </strong>{contatoDetalhe.estado}</p>
-                  <p><strong>Latitude: </strong>{contatoDetalhe.tipo}</p>
-                  <p><strong>Longitude: </strong>{contatoDetalhe.tipo}</p>
-                  <p><strong>Vazão: </strong>{contatoDetalhe.idade}</p>
-                  <p><strong>Pressão: </strong>{contatoDetalhe.idade}</p>
-                  <p><strong>Data da última vistoria: </strong>{contatoDetalhe.idade}</p>
-                  <p><strong>Tipo: </strong>{contatoDetalhe.idade}</p>
-                  <p><strong>Observação: </strong>{contatoDetalhe.idade}</p>
+                  <p><strong>Nome: </strong>{contatoDetalhe?.nome}</p>
+                  <p><strong>Logradouro: </strong>{contatoDetalhe?.endereco}</p>
+                  <p><strong>Bairro: </strong>{contatoDetalhe?.bairro}</p>
+                  <p><strong>Cidade: </strong>{contatoDetalhe?.cidade}</p>
+                  <p><strong>Estado: </strong>{contatoDetalhe?.estado}</p>
+                  <p><strong>Latitude: </strong>{contatoDetalhe?.latitude}</p>
+                  <p><strong>Longitude: </strong>{contatoDetalhe?.longitude}</p>
+                  <p><strong>Vazão: </strong>{contatoDetalhe?.vazao}</p>
+                  <p><strong>Pressão: </strong>{contatoDetalhe?.pressao}</p>
+                  <p><strong>Data da última vistoria: </strong>{contatoDetalhe?.datadaultimavistoria}</p>
+                  <p><strong>Tipo: </strong>{contatoDetalhe?.tipo}</p>
+                  <p><strong>Observação: </strong>{contatoDetalhe?.observacao}</p>
                 </div>
                 <div className='mt-5'>
                   <p className='text-center'>Opções hidrantes:</p>
                 </div>
                 <div className='flex justify-center'>
-                  <Link href={`/contatos/${contatoDetalhe.id}/editar`} className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100  focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>Editar</Link>
-                  <Link href={`/contatos/${contatoDetalhe.id}/excluir`} className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100  focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'> Excluir</Link>
+                  <Link href={`/contatos/${contatoDetalhe?.id}/editar`} className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>Editar</Link>
+                  <Link href={`/contatos/${contatoDetalhe?.id}/excluir`} className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>Excluir</Link>
                 </div>
               </ModalBody>
               <ModalFooter>
@@ -87,7 +84,7 @@ export default function Page() {
       </Modal>
 
       <h2 className="text-4xl text-center font-bold dark:text-white">Hidrantes Disponíveis</h2>
-      <p className="text-center	my-4 text-lg text-gray-500">Clique no botão <strong>detalhes</strong> para obter mais opções</p>
+      <p className="text-center my-4 text-lg text-gray-500">Clique no botão <strong>detalhes</strong> para obter mais opções</p>
 
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <div className="relative">
@@ -99,24 +96,23 @@ export default function Page() {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3 text-center">Mais Opções</th>
-              <th scope="col" className="px-6 py-3 ">Logradouro</th>
+              <th scope="col" className="px-6 py-3">Logradouro</th>
               <th scope="col" className="px-6 py-3 text-center">Bairro</th>
               <th scope="col" className="px-6 py-3 text-center">Tipo</th>
             </tr>
           </thead>
           <tbody>
-            {
-              contatos.map((contato) =>
-                <tr key={contato.id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                  <td className="px-6 py-4 flex justify-center">
-                    <Button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 hover:text-white focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onClick={() => { setContatoDetalhe(contato); onOpen() }}>Detalhes</Button>
-                    <Link href='' className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100  focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'> Localizar</Link>
-                  </td>
-                  <td className="px-6 py-4">{contato.logradouro}</td>
-                  <td className="px-6 py-4">{contato.cidade}</td>
-                  <td className="px-6 py-4">{contato.tipo}</td>
-                </tr>
-              )}
+            {contatos.map((contato) => (
+              <tr key={contato.id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <td className="px-6 py-4 flex justify-center">
+                  <Button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 hover:text-white focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onClick={() => { setContatoDetalhe(contato); onOpen(); }}>Detalhes</Button>
+                  <Link href='' className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'> Localizar</Link>
+                </td>
+                <td className="px-6 py-4">{contato.logradouro}</td>
+                <td className="px-6 py-4">{contato.cidade}</td>
+                <td className="px-6 py-4">{contato.tipo}</td>
+              </tr>
+            ))}
           </tbody>
           <tfoot>
             <tr>
@@ -125,7 +121,6 @@ export default function Page() {
           </tfoot>
         </table>
       </div>
-
     </div>
-  )
+  );
 }
--- src/authMiddleware.js
+++ src/authMiddleware.js
@@ -1,11 +1,22 @@
-import { headers } from '@/next.config';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-const chaveSecreta = process.env.JWT_SECRET ?? '123123'
+const chaveSecreta = process.env.JWT_SECRET ?? '123123';
 
 const rotasPublicas = [
     { path: '/api/contatos', method: 'GET' }
-]
+];
+
+const rotasPrivadas = [
+    { path: '/api/usuarios', method: 'GET', allowedProfiles: ['Administrador'] },
+    { path: '/api/usuarios', method: 'POST', allowedProfiles: ['Administrador'] },
+    { path: '/api/usuarios', method: 'PUT', allowedProfiles: ['Administrador'] },
+    { path: '/api/usuarios', method: 'DELETE', allowedProfiles: ['Administrador'] },
+
+    { path: '/api/contatos', method: 'GET', allowedProfiles: ['Administrador', 'Padrão'] },
+    { path: '/api/contatos', method: 'POST', allowedProfiles: ['Administrador'] },
+    { path: '/api/contatos', method: 'PUT', allowedProfiles: ['Administrador', 'Padrão'] },
+    { path: '/api/contatos', method: 'DELETE', allowedProfiles: ['Administrador', 'Padrão'] }
+];
 
 export default function authMiddleware(handler) {
     return (req, res) => {
@@ -26,13 +37,24 @@ export default function authMiddleware(handler) {
         }
 
         try {
-            const { username } = jwt.verify(token, chaveSecreta)
+            const { username, perfil } = jwt.verify(token, chaveSecreta);
             req.username = username;
+            req.perfil = perfil;
+
+            const rotaPrivada = rotasPrivadas.find(
+                (rota) => req.url.startsWith(rota.path) && req.method === rota.method
+            );
+
+            if (rotaPrivada && !rotaPrivada.allowedProfiles.includes(perfil)) {
+                res.status(403).end();
+                return;
+            }
+
         } catch {
-            res.status(401).end()
+            res.status(401).end();
             return;
         }
 
         return handler(req, res);
-    }
+    };
 }
